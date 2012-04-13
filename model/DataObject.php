126d1c3b3b905335b0ecc6c8b5a618e380eb21ef
Mateusz Uzdowski <mateusz@silverstripe.com>
2012-04-13 16:18:06 +1200

    APICHANGE: add validation extension hook to DataExtension

--- model/DataObject.php
+++ model/DataObject.php
@@ -882,7 +882,7 @@ class DataObject extends ViewableData implements DataObjectInterface, i18nEntity
 	 * Validate the current object.
 	 *
 	 * By default, there is no validation - objects are always valid!  However, you can overload this method in your
-	 * DataObject sub-classes to specify custom validation.
+	 * DataObject sub-classes to specify custom validation, or use the hook through DataExtension.
 	 * 
 	 * Invalid objects won't be able to be written - a warning will be thrown and no write will occur.  onBeforeWrite()
 	 * and onAfterWrite() won't get called either.
@@ -893,7 +893,9 @@ class DataObject extends ViewableData implements DataObjectInterface, i18nEntity
 	 * @return A {@link ValidationResult} object
 	 */
 	protected function validate() {
-		return new ValidationResult();
+		$result = new ValidationResult();
+		$this->extend('validate', $result);
+		return $result;
 	}
 
 	/**
